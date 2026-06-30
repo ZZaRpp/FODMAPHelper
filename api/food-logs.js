0380245e9cc6973c8befa904f7939574f0e9939f
ZZaRpp <neozarp@gmail.com>
2026-06-30 19:02:19 +0200

    Update food-logs.js

--- api/food-logs.js
+++ api/food-logs.js
@@ -17,9 +17,11 @@ function verifyToken(token) {
 }
 
 export default async function handler(req, res) {
+  // CORS headers - allow all origins
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
